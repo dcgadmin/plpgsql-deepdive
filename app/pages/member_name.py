fee0e74cb7da11ca2355b84bca33ac3afdf9fe81
DataCloudGaze <137620464+dcgadmin@users.noreply.github.com>
2025-02-28 23:10:36 +0530

    Update member_name.py

--- app/pages/member_name.py
+++ app/pages/member_name.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from executor import get_member_name
 
-limit = st.number_input("Enter a limit (Max: 500000)", min_value=1, max_value=500000, value=500000, step=1)
+limit = st.number_input("Enter a limit (Max: 1000000)", min_value=1, max_value=1000000, value=500000, step=1)
 if limit:
-    if limit <= 500000:
+    if limit <= 1000000:
         if st.button("Member names", type="primary"):
             execution_time = get_member_name(limit)
             if execution_time is not None:

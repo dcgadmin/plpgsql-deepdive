15a2312316a55caadfa0c8bc45e523299b710a3a
DataCloudGaze <137620464+dcgadmin@users.noreply.github.com>
2025-03-01 13:37:50 +0530

    Update search_bookings.py

--- app/pages/search_bookings.py
+++ app/pages/search_bookings.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from executor import get_individual_bookings
 
-name = st.text_input("Column Name", placeholder="Enter column name").strip()
-surname = st.text_input("Name", placeholder="Enter name").strip()
+name = st.text_input("Member Name(firstname or surname) Column", placeholder="Enter firstname or surname column name").strip()
+surname = st.text_input("Input Name ", placeholder="Enter name to search").strip()
 
 if st.button("Search Bookings"):
     details = get_individual_bookings(name, surname)

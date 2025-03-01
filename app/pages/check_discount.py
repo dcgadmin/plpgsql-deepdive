ac514c4ae6cd7fb6d87b0637d9d325144a8a4beb
Akash <akashgupta0419@gmail.com>
2025-03-01 13:17:29 +0530

    Added signs for checking discount

--- app/pages/check_discount.py
+++ app/pages/check_discount.py
@@ -7,4 +7,6 @@ if members_list is not None:
     selected_member = st.selectbox("Select Member:", members_list, index=None, placeholder="Select member name...")
     if selected_member is not None:
         data = get_discounts(selected_member)
+        bool_cols = data.select_dtypes(include=[bool]).columns
+        data[bool_cols] = data[bool_cols].replace({True: "✅", False: "❌"})
         st.dataframe(data, use_container_width=True, hide_index=True)

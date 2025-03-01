4081622752749dda55c41534e009f0856c2c8c82
DataCloudGaze <137620464+dcgadmin@users.noreply.github.com>
2025-03-01 20:00:51 +0530

    Update check_discount.py

--- app/pages/check_discount.py
+++ app/pages/check_discount.py
@@ -9,4 +9,5 @@ if members_list is not None:
         data = get_discounts(selected_member)
         bool_cols = data.select_dtypes(include=[bool]).columns
         data[bool_cols] = data[bool_cols].replace({True: "✅", False: "❌"})
+        st.write(f"Discount Applicability for Member : {selected_member}")
         st.dataframe(data, use_container_width=True, hide_index=True)

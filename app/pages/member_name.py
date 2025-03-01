5ba630a4e72af8d75aa1c2cd49ff92059e97e092
DataCloudGaze <137620464+dcgadmin@users.noreply.github.com>
2025-03-01 14:37:50 +0530

    Update member_name.py

--- app/pages/member_name.py
+++ app/pages/member_name.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from executor import get_member_name
 
-st.write("SQL Running in Database - with alias1 as NOT MATERIALIZED (select get_member_name(mod(generate_series,50)) from generate_series(1,%s)) select count(1) from alias1;)"
+st.write("SQL Running in Database - with alias1 as NOT MATERIALIZED (select get_member_name(mod(generate_series,50)) from generate_series(1,%s)) select count(1) from alias1;")
 limit = st.number_input("Enter a limit (Max: 1000000)", min_value=1, max_value=1000000, value=500000, step=1)
 if limit:
     if limit <= 1000000:

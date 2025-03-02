41e2b68f76f3cae9eee06debf1d3a940410506a8
Deepak Mahto <52906469+dmahto05@users.noreply.github.com>
2025-03-02 11:49:06 +0530

    Update all_facilities.py

--- app/pages/all_facilities.py
+++ app/pages/all_facilities.py
@@ -2,7 +2,7 @@ import streamlit as st
 from executor import get_all_facilities
 
 
-limit = st.number_input("Enter Rows (Max: 50)", min_value=1, max_value=50, value=50, step=1)
+limit = st.number_input("Enter Rows (Max: 10)", min_value=1, max_value=10, value=5, step=1)
 if limit:
     if st.button("Search", type="primary"):
         data = get_all_facilities(limit)
